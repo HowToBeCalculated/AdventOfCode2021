b3413a30043ec0ea835ecd4f2a91393184494650
HowToBeCalculated <HowToBeCalculated@outlook.com>
2021-12-01 22:50:43 -0500

    answered part two as well

---
+++ Day_01/Day_01_pt2.py
@@ -0,0 +1,24 @@
+from typing import List
+
+from get_digits_from_file import get_digits_from_file
+from Day_01_pt1 import count_increases
+
+def get_three_in_row_sums(array: list) -> List[int]:
+
+    #zip the 3 important elements to sum
+    zipped_arrays = zip(array[:-2], array[1:-1], array[2:])
+
+    #sum the triple elements
+    summed_array = map(sum, zipped_arrays)
+
+    #expand to list
+    return list(summed_array)
+
+#get a list of inputs from the file given
+inputArray = get_digits_from_file("Day_01_pt2_input.txt")
+
+#follow the directions to sum up the triple rows
+summed_array = get_three_in_row_sums(inputArray)
+
+#print the answer
+print(count_increases(summed_array))
