a938f2ba45fcc6831fe2105eae8546367e13bd1b
HowToBeCalculated <HowToBeCalculated@outlook.com>
2021-12-03 14:30:23 -0500

    successfully finished part 1 of day 3

---
+++ Day_03/Day_03_pt1.py
@@ -0,0 +1,76 @@
+from typing import List, Iterator, Tuple
+
+from extract_binary_from_file import extract_binary_from_file
+
+def get_common_binary_of_each_index(binaryArry: List[List[int]]) -> List[int]:
+
+    #use the first element as a proxy for the length of each element
+    lengthOfEachElement = len(binaryArray[0])
+
+    #record the number of elements in an array
+    numberOfElements = len(binaryArray)
+
+    #create a dictionary to store values and initialize each value to 0
+    indexDict = {index: 0 for index in range(lengthOfEachElement)}
+
+    #iterate through each binary list
+    for listElement in binaryArray:
+
+        #iterate through each element
+        for index, element in enumerate(listElement):
+
+            #update the index total value
+            indexDict[index] += element
+
+    #initialize a list to store the common binaries
+    commonBinary = []
+
+    #iterate through each index to calculate and store
+    for index in range(lengthOfEachElement):
+
+        #get index sum
+        indexSum = indexDict[index]
+
+        #get the average by dividing by number of list elements
+        indexAvg = indexSum / numberOfElements
+
+        #round the result and it will give the common binary
+        common = int(round(indexAvg, 0))
+
+        #append to our list
+        commonBinary.append(common)
+
+    return commonBinary
+
+def reverse_binary_list(binaryList: List[int]) -> List[int]:
+    
+    #reverse the binary array
+    reversedBinaryList = [abs(binary - 1) for binary in binaryList]
+    
+    return reversedBinaryList
+
+def convert_binary_list_to_int(binaryList: List[int]) -> List[int]:
+    
+    #make binary value into string for processing
+    stringValue = "".join([str(binary) for binary in binaryList])
+
+    #use python's built-in casting to convert binary to int
+    convertedValue = int(stringValue, 2)
+
+    return convertedValue
+
+if __name__ == "__main__":
+
+    #extract our data
+    binaryArray = extract_binary_from_file("Day_03_input.txt")
+
+    #process the common digits
+    commonBinary = get_common_binary_of_each_index(binaryArray)
+    reversedBinary = reverse_binary_list(commonBinary)
+
+    #convert both binaries to ints
+    commonInteger = convert_binary_list_to_int(commonBinary)
+    reservedInteger = convert_binary_list_to_int(reversedBinary)
+
+    #produce the answer
+    print("Answer:", commonInteger * reservedInteger)
--- Day_03/extract_binary_from_file.py
+++ Day_03/extract_binary_from_file.py
@@ -2,13 +2,19 @@ from typing import List
 
 def clean_binary_from_string(string: str) -> str:
 
-    #retain all numeric characters and join then together
+    #retain the binary number after removing the new line
     stringOfBinary = string.replace("\n", "")
-    
-    #return the string of digits casted to integer
+
     return stringOfBinary
 
-def extract_binary_from_file(file_name: str) -> List[str]:
+def make_numeric_array(binaryString : str) -> List[int]:
+
+    #separate out each character and cast as integer
+    integerArray = [int(char) for char in binaryString]
+
+    return integerArray
+
+def extract_binary_from_file(file_name: str) -> List[List[int]]:
 
     #initialize an array to store values
     binaryArray = []
@@ -16,7 +22,8 @@ def extract_binary_from_file(file_name: str) -> List[str]:
     #retrieve all of the lines
     with open(file_name) as file:
         for line in file:
-            binaryArray.append(clean_binary_from_string(line))
+            cleaned_string = clean_binary_from_string(line)
+            binaryArray.append(make_numeric_array(cleaned_string))
 
     return binaryArray
 
